a8037870b8fb0a34e8ff897563ec21ff9e4d0078
Steven Swee <sswee@dt-login01.delta.ncsa.illinois.edu>
2025-02-23 16:53:03 -0600

    Change file structure

---
+++ knowledge_distillation.py
@@ -0,0 +1,37 @@
+# os
+import os
+from tqdm import tqdm
+
+# data handling
+import numpy as np
+import pandas as pd
+
+# tensor
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+
+# transformers
+from transformers import AutoModel, AutoTokenizer
+
+# helper functions
+from KD.text_embedding import get_cls_embedding
+
+
+if __name__ == '__main__':
+
+    # Load BioBERT Model
+    tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-v1.1")
+    model = AutoModel.from_pretrained("dmis-lab/biobert-v1.1")
+    
+    # Toy dataset
+    texts = ["Heart failure detected.", "No signs of cardiovascular issues.", "Possible arrhythmia found."]
+    labels = [1, 0, 1]  # 1: Disease, 0: No Disease
+
+    # Convert texts to embeddings
+    embeddings = torch.cat([get_cls_embedding(text) for text in texts]).numpy()
+    
+    # Test
+    print("Success!")
