c6165629b307f0f4fe859487e05453cd76d1ec6e
Steven Swee <sswee@dt-login04.delta.ncsa.illinois.edu>
2025-02-22 22:21:21 -0600

    Job

---
+++ NOTEBOOKS FOR RUN/Partition.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=LLM_testing     # Job name
+#SBATCH --output=/projects/bdlo/download_output.log  # Output log file
+#SBATCH --error=/projects/bdlo/download_error.log    # Error log file
+#SBATCH --time=1:00:00                    # Max runtime (hh:mm:ss)
+#SBATCH --ntasks=1                         # Number of tasks (single wget process)
+#SBATCH --cpus-per-task=1                  # Single-core usage (wget is not CPU-intensive)
+#SBATCH --mem=1G                           # Memory allocation (1GB should be sufficient)
+#SBATCH --partition=gpuA100x4                 # Partition to submit to (adjust based on your system)
+#SBATCH --account=bdlo-delta-gpu           # Specify the correct account
+
+# Load necessary modules
+module load cuda/11.8 # Load CUDA
+
+# Test LLM
+python LLM_Testing.py
